6259e2e256b6d78ef0b9551707019fa217dcb916
Dana Zagha <dana.zagha0@gmail.com>
2023-05-04 15:09:22 +0300

    step4

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -5,9 +5,21 @@ namespace TestProject
     public class UnitTest1
     {
         [Theory]
+        [InlineData("//;\n2;3;1;5", 11)]
+        public void SupportDifferentDelimiters_Test(string nums, int expected)
+        {
+            //Act
+            var numbers = new StringCalculatorService();
+            int actual = numbers.Add(nums);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
         [InlineData("1\n2,3", 6)]
         [InlineData("1,2,3", 6)]    
-        public void SupportDifferentDelimiters_Test(string nums, int expected)
+        public void HandleNewLinesBetweenNums_Test(string nums, int expected)
         {
             //Act
             var numbers = new StringCalculatorService();
